0ceef47cd04f1a99f38198150e1f8cdc748bf632
hiquanta <chuanxuelingfeng@gmail.com>
2016-10-19 20:40:41 +0800

    add BaseListFragment

---
+++ app/src/main/java/com/hiquanta/scaffold/view/fragment/BaseListFragment.java
@@ -0,0 +1,11 @@
+package com.hiquanta.scaffold.view.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by hiquanta on 2016/10/19.
+ */
+
+public abstract class BaseListFragment extends Fragment{
+
+}
